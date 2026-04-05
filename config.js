ec9d2021da0b47ad9e4709f3153ba79db6ea20ca
laurentarsene <arsene.laurent@protonmail.com>
2026-04-05 21:56:24 +0200

    config: credentials Supabase ECWaterloo

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 //  À remplir après création du projet sur supabase.com
 // ═══════════════════════════════════════════════════════
 
-const SUPABASE_URL      = 'https://VOTRE_PROJECT_ID.supabase.co';
-const SUPABASE_ANON_KEY = 'VOTRE_ANON_KEY';
+const SUPABASE_URL      = 'https://vzfrrhvifoaaupzqicbs.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6ZnJyaHZpZm9hYXVwenFpY2JzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU0MTI0NzQsImV4cCI6MjA5MDk4ODQ3NH0.HiZ3h941BQGVmIDO2ZGQ7oJh93h9gC9Lm1aaYzDQbxM';
